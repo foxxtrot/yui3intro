df6abe4fc566872d3f7a2905c5441d32ece1bc76
Jeff Craig <foxxtrot@foxxtrot.net>
2010-05-04 22:57:43 -0700

    Support for filtering list.

--- checkin/views.py
+++ checkin/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import render_to_response
 from pcc2010.checkin.models import Registration
+from django.db.models import Q
 
 def index(request):
-    registration_list = Registration.objects.all().order_by('full_name')
-    return render_to_response('checkin/index.html', {'registration_list': registration_list})
+    registration_list = Registration.objects.filter(Q(full_name__contains = request.session['filter_text']) | Q(email__contains = request.session['filter_text'])).order_by('full_name')
+    return render_to_response('checkin/index.html', {'registration_list': registration_list, 'filter_text': request.session['filter_text']})
 
 def checkin(request, registration_id):
     try:
@@ -14,3 +15,8 @@ def checkin(request, registration_id):
     r.checkedin = True
     r.save()
     return HttpResponseRedirect('/checkin/')
+
+def filter(request):
+    request.session['filter_text'] = request.POST['filter_text']
+    return HttpResponseRedirect('/checkin/')
+
