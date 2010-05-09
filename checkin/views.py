b91d90d0979f43b506a866c7966bba5da9c2058b
Jeff Craig <foxxtrot@foxxtrot.net>
2010-05-09 15:20:58 -0700

    Rework of filtering.

--- checkin/views.py
+++ checkin/views.py
@@ -3,13 +3,23 @@ from django.shortcuts import render_to_response
 from pcc2010.checkin.models import Registration
 from django.db.models import Q
 
-def index(request):
-    try:
-        ft = request.session['filter_text']
-    except KeyError:
-        ft = ""
-    registration_list = Registration.objects.filter(Q(full_name__contains = ft) | Q(email__contains = ft)).order_by('full_name')
-    return render_to_response('checkin/index.html', {'registration_list': registration_list, 'filter_text': ft})
+PAGE_LENGTH = 25
+
+def index(request, page_number=1):
+    page_number = int(page_number)
+    registration_list = Registration.objects.order_by('full_name').all()
+    view_data = {
+		    'registration_list': registration_list[(page_number-1) * PAGE_LENGTH:(page_number * PAGE_LENGTH) - 1],
+		    'filter_text': ""
+		}
+    if page_number > 1:
+	    view_data["prev_page"] = page_number - 1
+    if page_number != len(registration_list) / PAGE_LENGTH:
+	    view_data["next_page"] = page_number + 1
+
+    view_data["link_prefix"] = "/checkin/page/";
+
+    return render_to_response('checkin/index.html', view_data);
 
 def checkin(request, registration_id):
     try:
@@ -18,9 +28,29 @@ def checkin(request, registration_id):
         raise Http404
     r.checkedin = True
     r.save()
-    return HttpResponseRedirect('/checkin/')
 
-def filter(request):
-    request.session['filter_text'] = request.POST['filter_text']
-    return HttpResponseRedirect('/checkin/')
+    
+    if request.META["HTTP_REFERER"]:
+        return HttpResponseRedirect(request.META["HTTP_REFERER"])
+    else:
+        return HttpResponseRedirect('/checkin/')
 
+def filter(request, filter_text = None, page_number = 1):
+    if filter_text == None:
+        return HttpResponseRedirect('/checkin/filter/%s' % request.POST['filter_text'])
+    
+    page_number = int(page_number)
+    
+    registration_list = Registration.objects.filter(Q(full_name__contains = filter_text) | Q(email__contains = filter_text)).order_by('full_name')
+    view_data = {
+		    'registration_list': registration_list[(page_number-1) * PAGE_LENGTH:(page_number * PAGE_LENGTH) - 1],
+		    'filter_text': filter_text
+		}
+    if page_number > 1:
+	    view_data["prev_page"] = page_number - 1
+    if page_number != len(registration_list) / PAGE_LENGTH:
+	    view_data["next_page"] = page_number + 1
+	
+    view_data["link_prefix"] = "/checkin/filter/%s/page/" % filter_text
+
+    return render_to_response('checkin/index.html', view_data);
--- urls.py
+++ urls.py
@@ -6,7 +6,10 @@ admin.autodiscover()
 urlpatterns = patterns('',
     (r'^checkin/$', 'pcc2010.checkin.views.index'),
     (r'^checkin/checkin/(?P<registration_id>\d+)$', 'pcc2010.checkin.views.checkin'),
+    (r'^checkin/filter/(?P<filter_text>[\w%\d]+)$', 'pcc2010.checkin.views.filter'),
+    (r'^checkin/filter/(?P<filter_text>[\w\d%]+)/page/(?P<page_number>\d+)$', 'pcc2010.checkin.views.filter'),
     (r'^checkin/filter/$', 'pcc2010.checkin.views.filter'),
+    (r'^checkin/page/(?P<page_number>\d+)$', 'pcc2010.checkin.views.index'),
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
