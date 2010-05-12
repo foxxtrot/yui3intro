f4e482562baed249b2725da66e689da141261b22
Jeff Craig <foxxtrot@foxxtrot.net>
2010-05-11 21:53:58 -0700

    Support for Previous, Next automation on links.

--- checkin/views.py
+++ checkin/views.py
@@ -15,10 +15,19 @@ def index(request, page_number=1):
 		    'registration_list': registration_list[(page_number-1) * PAGE_LENGTH:(page_number * PAGE_LENGTH) - 1],
 		    'filter_text': ""
 		}
+
     view_data["prev_page"] = getPrevPageNumber(page_number)
     view_data["next_page"] = getNextPageNumber(page_number, len(registration_list), PAGE_LENGTH)
     view_data["link_prefix"] = "/checkin/page/";
 
+    if request.is_ajax():
+	    registrations_json = serializers.serialize('json', view_data["registration_list"]);
+	    jsonText = json.dumps({
+				'nextPage': view_data["next_page"],
+				'prevPage': view_data["prev_page"]
+			})
+	    jsonText = jsonText[0:len(jsonText)-1] + ',"data":' + registrations_json + '}'
+	    return HttpResponse(jsonText)
     return render_to_response('checkin/index.html', view_data);
 
 def checkin(request, registration_id):
@@ -28,7 +37,7 @@ def checkin(request, registration_id):
         raise Http404
     r.checkedin = True
     r.save()
-    
+
     if request.is_ajax():
 	    return HttpResponse(json.dumps({'id': registration_id, 'checkedin': True}))
     else:
@@ -40,9 +49,9 @@ def checkin(request, registration_id):
 def filter(request, filter_text = None, page_number = 1):
     if filter_text == None:
         return HttpResponseRedirect('/checkin/filter/%s' % request.POST['filter_text'])
-    
+
     page_number = int(page_number)
-    
+
     registration_list = Registration.objects.filter(Q(full_name__contains = filter_text) | Q(email__contains = filter_text)).order_by('full_name')
     view_data = {
 		    'registration_list': registration_list[(page_number-1) * PAGE_LENGTH:(page_number * PAGE_LENGTH) - 1],
@@ -53,4 +62,12 @@ def filter(request, filter_text = None, page_number = 1):
     view_data["next_page"] = getNextPageNumber(page_number, len(registration_list), PAGE_LENGTH)
     view_data["link_prefix"] = "/checkin/filter/%s/page/" % filter_text
 
+    if request.is_ajax():
+	    registrations_json = serializers.serialize('json', view_data["registration_list"]);
+	    jsonText = json.dumps({
+				'nextPage': view_data["next_page"],
+				'prevPage': view_data["prev_page"]
+			})
+	    jsonText = jsonText[0:len(jsonText)-1] + ',"data":' + registrations_json + '}'
+	    return HttpResponse(jsonText)
     return render_to_response('checkin/index.html', view_data);
--- static/js/checkin.js
+++ static/js/checkin.js
@@ -1,10 +1,51 @@
-YUI().use('node', 'io', function(Y) {
+YUI().use('node', 'io', 'json-parse', 'substitute', function(Y) {
+	var nextLink = Y.one('#next'),
+	    prevLink = Y.one('#prev'),
+	    checkinList = Y.one('#checkinlist'),
+	    rebuildList = function(id, response) {
+		var data = Y.JSON.parse(response.responseText),
+		    baseHref = location.pathname.replace(/\/page\/\d+$/, ""),
+		    i, html;
+		if (data.prevPage) {
+			prevLink.removeClass('hidden');
+			prevLink.set('href', baseHref + '/page/' + data.prevPage);
+		} else {
+			prevLink.addClass('hidden');
+		}
+		if (data.nextPage) {
+			nextLink.removeClass('hidden');
+			nextLink.set('href', baseHref + '/page/' + data.nextPage);
+		} else {
+			nextLink.addClass('hidden');
+		}
+		html = "";
+		for( i = 0 ; i < data.data.length ; i += 1 ) {
+			data.data[i].fields.id = data.data[i].pk;
+			html += Y.substitute("<li class={checkedin}><a class='checkin_link' href='/checkin/checkin/{id}'>{full_name} &lt;{email}&gt;</a></li>", data.data[i].fields, function(key, value) {
+				if (key === 'checkedin') {
+					return value ? 'checkedin' : '';
+				}
+				return value;
+			});
+		}
+		checkinList.setContent(html);
+	};
 	Y.on('click', function(ev) {
 		Y.log('Next clicked!');
+		Y.io(ev.target.get('href'), {
+			on: {
+				success: rebuildList
+			}
+		});
 		ev.preventDefault();
 	}, "#next");
 	Y.on('click', function(ev) {
 		Y.log('Previous clicked!');
+		Y.io(ev.target.get('href'), {
+			on: {
+				success: rebuildList
+			}
+		});
 		ev.preventDefault();
 	}, "#prev");
 
@@ -17,5 +58,5 @@ YUI().use('node', 'io', function(Y) {
 				}
 			}
 		});
-	}, '#checkinlist', 'a.checkin_link'); 
+	}, '#checkinlist', 'a.checkin_link');
 });
