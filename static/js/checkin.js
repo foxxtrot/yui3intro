4e79a411f7831393673e28d5b57250c3f26e0d4a
Jeff Craig <foxxtrot@foxxtrot.net>
2010-10-28 22:39:52 -0700

    Update for SimpleYUI

--- static/js/checkin.js
+++ static/js/checkin.js
@@ -1,84 +1,82 @@
-YUI().use('node', 'io', 'json-parse', 'substitute', function(Y) {
-    var nextLink = Y.one('#next'),
-        prevLink = Y.one('#prev'),
-        checkinList = Y.one('#checkinlist'),
-        rebuildList = function(id, response) {
+var nextLink = Y.one('#next'),
+    prevLink = Y.one('#prev'),
+    checkinList = Y.one('#checkinlist'),
+    rebuildList = function(id, response) {
         var data = Y.JSON.parse(response.responseText),
             baseHref = location.pathname.replace(/\/page\/\d+$/, ""),
             i, html;
         if (data.prevPage) {
-            prevLink.removeClass('hidden');
-            prevLink.set('href', baseHref + '/page/' + data.prevPage);
+          prevLink.removeClass('hidden');
+          prevLink.set('href', baseHref + '/page/' + data.prevPage);
         } else {
-            prevLink.addClass('hidden');
+          prevLink.addClass('hidden');
         }
         if (data.nextPage) {
-            nextLink.removeClass('hidden');
-            nextLink.set('href', baseHref + '/page/' + data.nextPage);
+          nextLink.removeClass('hidden');
+          nextLink.set('href', baseHref + '/page/' + data.nextPage);
         } else {
-            nextLink.addClass('hidden');
+          nextLink.addClass('hidden');
         }
         html = "";
         Y.Array.each(data.data, function(entry) {
-            entry.fields.id = entry.pk;
-            html += Y.substitute("<li class={checkedin}><a class='checkin_link' href='/checkin/checkin/{id}'>{full_name} &lt;{email}&gt;</a></li>",
-                entry.fields,
-                function(key, value) {
-                    if (key === 'checkedin') {
-                        return value ? 'checkedin' : '';
-                    }
-                    return value;
-                });
-        })
+          entry.fields.id = entry.pk;
+          html += "<li class=" + (entry.fields.checkedin ? 'checkedin' : '') +
+                  "><a class='checkin_link' href='/checkin/checkin/" +
+                  entry.pk + "'>" + entry.fields.full_name + " &lt;" +
+                  entry.fields.email + "&gt;</a></li>";
+        });
         checkinList.setContent(html);
     };
-    nextLink.on('click', function(ev) {
-        Y.log('Next clicked!');
-        Y.io(ev.target.get('href'), {
-            on: {
-                success: rebuildList
-            }
-        });
-        ev.preventDefault();
-    });
-    prevLink.on('click', function(ev) {
-        Y.log('Previous clicked!');
-        Y.io(ev.target.get('href'), {
-            on: {
-                success: rebuildList
-            }
-        });
-        ev.preventDefault();
-    });
 
-    Y.on('keyup', function(ev) {
-        var value = ev.target.get('value'), url;
-        Y.log('Change Event: ' + value);
-        if (value !== '') {
-            url = '/checkin/filter/' + value;
-        } else {
-            url = '/checkin/';
-        }
-        Y.io(url, {
-            on: {
-                success: rebuildList
-            }
-        });
-    }, "#filter_text");
-
-    Y.one('#filter_text').get('parentNode').one('input[type=submit]').remove();
-    Y.delegate("click", function(ev) {
-        ev.preventDefault();
-        Y.io(ev.target.get('href'), {
-            on: {
-                success: function(id, response) {
-                    var parent = ev.target.get('parentNode').addClass('checkedin');
-                }
-            }
-        });
-    }, '#checkinlist', 'a.checkin_link');
+nextLink.on('click', function(ev) {
+  Y.log('Next clicked!');
+  Y.io(ev.target.get('href'), {
+    on: {
+      success: rebuildList
+    }
+  });
+  ev.preventDefault();
 });
 
+prevLink.on('click', function(ev) {
+  Y.log('Previous clicked!');
+  Y.io(ev.target.get('href'), {
+    on: {
+      success: rebuildList
+    }
+  });
+  ev.preventDefault();
+});
+
+Y.on('keyup', function(ev) {
+  var value = ev.target.get('value'), url;
+  Y.log('Change Event: ' + value);
+  if (value !== '') {
+    url = '/checkin/filter/' + value;
+  } else {
+    url = '/checkin/';
+  }
+  Y.io(url, {
+    on: {
+      success: rebuildList
+    }
+  });
+}, "#filter_text");
+
+Y.one('#filter_text').get('parentNode').one('input[type=submit]').remove();
+
+Y.delegate("click", function(ev) {
+  ev.preventDefault();
+  Y.io(ev.target.get('href'), {
+    on: {
+      success: function(id, response) {
+        var parent = ev.target.get('parentNode').addClass('checkedin');
+      }
+    }
+  });
+}, '#checkinlist', 'a.checkin_link');
+
+/*
 YUI().use('node', 'json-parse', 'yui2-progressbar', 'gallery-io-poller', 'overlay', 'substitute', function(Y) {
     var container = Y.Node.create("<div id='progress'></div>"),
         progress, overlay;
@@ -109,3 +107,4 @@ YUI().use('node', 'json-parse', 'yui2-progressbar', 'gallery-io-poller', 'overla
         overlay.hide();
     });
 });
+*/
